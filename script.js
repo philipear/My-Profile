e39f7f129b24f397603856c15788f6b998f9e029
Philipe Araujo <philipe.araujo89@gmail.com>
2023-07-23 10:34:14 -0300

    Evento de click
    Alert

---
+++ script.js
@@ -0,0 +1,10 @@
+const meuGithub = document.querySelector("#rede1");
+const meuLikedin = document.querySelector("#rede2");
+
+meuGithub.addEventListener("click", () => {
+  alert("Você será direcionado para meu GIthub");
+});
+
+meuLikedin.addEventListener("click", () => {
+  alert("Você será direcionado para meu Likedin");
+});
